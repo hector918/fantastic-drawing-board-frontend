c46b63f6605bab59ef454873f752e8a817b0f0b2
Hector Zhong <hector_zhong@outlook.com>
2023-02-15 19:59:09 -0500

    daliy update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DrawingTile from "../components/drawing-tile";
 import { srv } from "../fetch_";
-
+import './index.css';
 
 ///////////////////////////////////////////////////////////////
 export default function Index({username}) {
@@ -11,22 +11,21 @@ export default function Index({username}) {
     srv.get_all_drawings((data) =>{
       if(Array.isArray(data)) setDrawings(data);
     })
+    document.querySelector(".red_span").classList.add("fade");
   },[])
   return (
     <div className="container">
       <div className="hero bg-gray hero-sm">
         <div className="hero-body">
           <div className="columns">
-            <div className="column">
-              <h1>Drawing board</h1>
+            <div className="column" style={{display: "inline-flex"}}>
+              <h1 style={{margin:"auto"}}>Draw<span className="red_span">i</span>ng board</h1>
             </div>
             <div className="divider-vert " data-content="|"></div>
             <div className="column">
-              <p className="text-left">I am a canvas, yet not for paint,
-An empty space, yet not for faint.<br></br>What am I, you may ask, this enigma of creation?
-A riddle that challenges the mind's exploration.
-I am a drawing board, a realm of possibility,
-A space where art and creativity find their ability.</p>
+              <p className="text-left">Behold, a canvas doth lie before thee,A space that calls for thy creativity.</p>
+              <p className="text-left">What may this be, thou may ask, this wondrous place? A riddle that doth challenge the mind's embrace. </p>
+              <p className="text-right"> - ChatGPT 3.5</p>
               <p className="text-right">
                 {username===undefined?
                   <Link to="/login">Login...</Link>
